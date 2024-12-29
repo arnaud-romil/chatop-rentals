a7a8ba4f0291168eee483876f02946d63d94e294
Arnaud ROMIL <arnaud.romil@gmail.com>
2024-12-29 15:00:48 +0100

    add auth login api

--- src/main/java/com/chatop/rentalsapi/controllers/AuthController.java
+++ src/main/java/com/chatop/rentalsapi/controllers/AuthController.java
@@ -1,9 +1,15 @@
 package com.chatop.rentalsapi.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.chatop.rentalsapi.models.LoginRequest;
 import com.chatop.rentalsapi.models.RegisterRequest;
 import com.chatop.rentalsapi.models.TokenResponse;
 import com.chatop.rentalsapi.models.User;
@@ -25,5 +31,22 @@ public class AuthController {
     public TokenResponse register(@RequestBody RegisterRequest registerRequest) {
         User user = userService.registerUser(registerRequest);
         return jwtService.generateToken(user);
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest) {
+        ResponseEntity<TokenResponse> result;
+        Optional<User> user = userService.login(loginRequest);
+        if (user.isPresent()) {
+            result = ResponseEntity.ok(jwtService.generateToken(user.get()));
+        } else {
+            result = ResponseEntity.status(401).build();
+        }
+        return result;
+    }
+
+    @GetMapping("/auth/me")
+    public String me(Authentication authentication) {
+        return "Hello " + authentication.getName();
     }
 }
--- src/main/java/com/chatop/rentalsapi/services/UserService.java
+++ src/main/java/com/chatop/rentalsapi/services/UserService.java
@@ -1,10 +1,12 @@
 package com.chatop.rentalsapi.services;
 
 import java.time.Instant;
+import java.util.Optional;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.chatop.rentalsapi.models.LoginRequest;
 import com.chatop.rentalsapi.models.RegisterRequest;
 import com.chatop.rentalsapi.models.User;
 import com.chatop.rentalsapi.repositories.UserRepository;
@@ -33,5 +35,20 @@ public class UserService {
         user.setCreatedAt(Instant.now());
         user.setUpdatedAt(Instant.now());
         return saveUser(user);
+    }
+
+    public Optional<User> login(LoginRequest loginRequest) {
+        Optional<User> result;
+        User user = userRepository.findByEmail(loginRequest.getLogin());
+        if (user != null && isPasswordCorrect(user, loginRequest)) {
+            result = Optional.of(user);
+        } else {
+            result = Optional.empty();
+        }
+        return result;
+    }
+
+    private boolean isPasswordCorrect(User user, LoginRequest loginRequest) {
+        return this.passwordEncoder.matches(loginRequest.getPassword(), user.getPassword());
     }
 }
